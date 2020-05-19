6c7ffecdffdf9910f7b092cac8ef6655c4526765
LeeChenSe <42361526+LiChenZe@users.noreply.github.com>
2020-05-19 22:34:04 +0800

    fetch chapter update of 2020-05-19

--- WEBFrontEnd/ECMAScript/AsyncHandler/Fetch/app.js
+++ WEBFrontEnd/ECMAScript/AsyncHandler/Fetch/app.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const server = express();
+const bodyParser = require('body-parser');
 var user = {
     lee : '111',
 }
 
+server.use(bodyParser.urlencoded({}))
 server.listen(9999);
+
+server.post('/loginPost', (req, res) => {
+    if(user[req.body.user] == req.body.pass){
+        res.send({'ok':1, msg: '登录成功'});
+    }else{
+        res.send({'ok':0, msg: '登录失败'});
+    }
+})
 
 server.use('/login', (req, res) => {
     if(user[req.query.user] == req.query.pass){
