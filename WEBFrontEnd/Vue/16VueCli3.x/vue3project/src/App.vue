18b5a9b06ec014a4c9f3e88cef7503a16e7468e4
LeeChenSe <42361526+LiChenZe@users.noreply.github.com>
2020-06-25 18:53:20 +0800

    2020-06-25

---
+++ WEBFrontEnd/Vue/16VueCli3.x/vue3project/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  data() {
+      return {
+        
+      }
+  },
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
