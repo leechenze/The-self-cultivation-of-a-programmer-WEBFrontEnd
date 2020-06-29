18959965911b23e954a9be4f072a025a0e9fd776
LeeChenSe <42361526+LiChenZe@users.noreply.github.com>
2020-06-29 23:32:39 +0800

    2020-06-29

--- WEBFrontEnd/ECMAScript/TypeScript/05TypeScript中的类/index.ts
+++ WEBFrontEnd/ECMAScript/TypeScript/05TypeScript中的类/index.ts
@@ -1,2 +1,24 @@
 // 静态属性, 静态方法;
+    class Person {
+
+        // 公共属性;
+        public name:string;
+        
+        // 构造函数;
+        constructor(name:string) {
+            this.name = name;   
+        }
+
+
+        // 实例方法
+        run() {
+            return `${this.name}在运动`;
+        }
+        eat() {
+            return `${this.name}在吃饭`;
+        }
+    }
+
+    var person = new Person('hilarie');
+    console.log(person.run());
     
