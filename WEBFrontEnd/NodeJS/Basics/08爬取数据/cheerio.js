4281e070a4a0ce96b3976e9cd15b61ec3a548ba0
LeeChenSe <42361526+LiChenZe@users.noreply.github.com>
2020-07-19 18:58:49 +0800

    2020-07-19

--- WEBFrontEnd/NodeJS/Basics/08爬取数据/cheerio.js
+++ WEBFrontEnd/NodeJS/Basics/08爬取数据/cheerio.js
@@ -15,39 +15,87 @@
 const cheerio = require('cheerio'),
     axios = require('axios'),
     fs = require('fs'),
-    url = require('url');
+    url = require('url'),
+    path = require('path');
 
 
-let httpUrl = 'https://www.doutula.com/article/list/?page=2';
 
-axios.get(httpUrl).then((res) => {
-    // console.log(res.data);
-    // cheerio解析html文档, 规定使用cheerio加载html文档, 即可使用$对象(cherrio == jquery);
+
+spider();
+// 爬取所有页面图片;
+async function spider() {
+    
+    // 获取页面的长度(有几个页面);
+    let httpUrl = 'https://www.doutula.com/article/list/?page=2';
+    let res = await axios.get(httpUrl);
     let $ = cheerio.load(res.data);
-    // 获取每一页中的a链接🔗模块;
-    $('#home .col-sm-9 a.list-group-item').each((ind, ele) => {
-        // 获取每一页中的a链接地址;
-        let pageUrl = $(ele).attr('href');
-        // 获取每一个a链接模块中的图片地址;
-        // parseLink(pageUrl, '.pic-content .artile_des img', 'src');
-    })
-})
+    let PageSize = $('.pagination li.page-item').eq($('.pagination li').length - 2).find('a').text();
+    
+    for(let i = 1; i <= parseInt(PageSize); i++) {
+        // getPageList(i);
+        // 因为页数太多所以502错误, 这里先执行一条;
+        if(i == 1){
+            getPageList(i);
+        }
+    }
+}
 
 
-// 参数为(请求地址, 要爬取的元素选择器, 要爬取的元素选择器的属性);
-async function parseLink(url, selectorArr, getAttr) {
-    let res = await axios.get(url);
-    let $ = cheerio.load(res.data);
-    $(selectorArr).each((ind, ele) => {
-        let imgUrl = $(ele).attr(getAttr);
-        console.log(imgUrl);
+// 请求每一页的数据
+function getPageList(pageNum) {
+
+    let httpUrl = 'https://www.doutula.com/article/list/?page='+pageNum;
+    
+    axios.get(httpUrl).then((res) => {
+        // console.log(res.data);
+        // cheerio解析html文档, 规定使用cheerio加载html文档, 即可使用$对象(cherrio == jquery);
+        let $ = cheerio.load(res.data);
+        // 获取每一页中的a链接🔗模块;
+        $('#home .col-sm-9 a.list-group-item').each((ind, ele) => {
+            // 获取每一页中的a链接地址;
+            let pageALinkUrl = $(ele).attr('href');
+            // 获取每一页中的a链接模块标题;
+            let pageALinktitle = $(ele).find('.random_title').text();
+            // 匹配所有无数次零个或n个为数字的字符;
+            let reg = /(.*?)\d/igs;
+            // reg匹配每一页中的a链接模块标题;
+            pageALinktitle = reg.exec(pageALinktitle)[1];
+            // 创建目录用来存储爬取到的图片;
+            fs.mkdir('./images/' + pageALinktitle, (err) => {
+                if(err) {
+                    throw Error(err);
+                }else{
+                    console.log(`创建${pageALinktitle}目录成功`);
+                }
+            });
+            // 获取每一个a链接模块中的图片地址;
+            parseLink(pageALinkUrl, '.pic-content .artile_des img', 'src', pageALinktitle);
+        })
     })
 }
 
 
 
-
-
-
-
+// 参数为(请求地址, 要爬取的元素选择器, 要爬取的元素选择器的属性);
+async function parseLink(url, selectorArr, getAttr, writeDirName) {
+    let res = await axios.get(url);
+    let $ = cheerio.load(res.data);
+    $(selectorArr).each((ind, ele) => {
+        // 获取图片链接;
+        let imgUrl = $(ele).attr(getAttr);
+        // 获取图片扩展名
+        let imgExtName = path.extname(imgUrl);
+        // 创建写入文件流(路径写入的路径和扩展名);
+        let ws = fs.createWriteStream(`./images/${writeDirName}/${writeDirName}-${ind}${imgExtName}`, {flag: 'a'});
+        // {responseType: 'stream'}这个参数规定以流的形式写入;
+        axios.get(imgUrl, {responseType: 'stream'}).then(function (res) {
+            res.data.pipe(ws);
+            // 监听当写入完成时关闭;
+            res.data.on('close',() => {
+                // 关闭写入流;
+                ws.close();
+            })
+        })
+    })
+}
 
