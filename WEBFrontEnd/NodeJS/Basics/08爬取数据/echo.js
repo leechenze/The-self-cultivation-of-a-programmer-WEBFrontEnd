354992b55d4185f6259ca79534c91539354934ae
LeeChenSe <42361526+LiChenZe@users.noreply.github.com>
2020-07-21 23:08:11 +0800

    2020-07-21

---
+++ WEBFrontEnd/NodeJS/Basics/08爬取数据/echo.js
@@ -0,0 +1,17 @@
+const axios = require('axios');
+
+
+
+// 获取音乐相关信息, 通过音乐相关信息获取音频地址;
+// 获取音乐列表, 通过音乐列表获取大量的音乐信息;
+
+
+// 通过音乐分类页, 获取音乐列表;
+async function getPage(num) {
+    let httpUrl = `https://www.app-echo.com/api/recommend/sound-day?page=${num}`;
+    
+}
+
+
+
+
