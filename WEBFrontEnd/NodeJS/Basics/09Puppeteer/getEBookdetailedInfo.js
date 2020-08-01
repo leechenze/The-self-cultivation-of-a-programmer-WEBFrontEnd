4fbbc76573e95ce76421117424a054b422b8230a
LeeChenSe <42361526+LiChenZe@users.noreply.github.com>
2020-08-01 16:27:13 +0800

    2020-08-01

---
+++ WEBFrontEnd/NodeJS/Basics/09Puppeteer/getEBookdetailedInfo.js
@@ -0,0 +1,20 @@
+let puppeteer = require('puppeteer');
+let axios = require('axios');
+
+
+
+
+
+// 目标: 获取https://sobooks.cc 所有电子书的书名和链接;
+
+
+// 进入网站, 获取整个网站的列表页总页数;
+
+
+// 获取哦列表页的所有链接;
+
+
+// 进入每个电子书的详情页,获取下载电子书的网盘地址;
+
+
+// 将获取的数据保存到本地(book.txt)文档中;
