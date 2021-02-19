094b69ff17c2b9fafd7fd2a6f1073200be7dce0b
LeeChenSe <42361526+LiChenZe@users.noreply.github.com>
2021-02-19 20:30:32 +0800

    modify showArea

--- WEBFrontEnd/React/reacthooks/src/ReducerExample/ShowArea.js
+++ WEBFrontEnd/React/reacthooks/src/ReducerExample/ShowArea.js
@@ -3,7 +3,6 @@ import {ColorContext} from './Color';
 
 export default function ShowArea() {
     const {color} = useContext(ColorContext);
-    console.log(color);
     return (
         <div style={{color: color}}>The FontStyle`s {color}</div>
     )
