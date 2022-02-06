0c56f2c68ecffc8d6a77be5f46189c4cd6d78e80
LeeChenZe <leeczyc@gmail.com>
2022-02-06 17:24:21 +0800

    webworker更新

---
+++ WEBFrontEnd/H5C3/HTML5/js/WebWorker/first-worker.js
@@ -0,0 +1,10 @@
+function fb(n) {
+  if (n == 1 || n == 2) {
+    return 1;
+  }
+  return fb(n - 1) + fb(n - 2);
+}
+console.time("FB执行时间");
+var result = fb(43);
+console.timeEnd("FB执行时间");
+self.postMessage("firstWorker FB执行完成");
